3b53910599256c6da817038278ea736b474d2336
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-03-31 16:28:44 +0200

    Restructure and plot random

---
+++ metrics/get_metrics.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+import metrics.metrics as M
+
+metric_keys = [
+    m for m in dir(M) if ((m[:2] != "__") & (m != "binarize") & (m != "normalize"))
+]
+metric_keys = [
+    "mse",
+    "sequest_score",
+    "andromeda_score",
+    "pearson_correlation",
+    "spearman_correlation",
+    "dot_product",
+    "mara_similarity",
+    "modified_dot_product",
+    "massbank_score",
+    "gnps_score",
+    "stein_scott_score",
+    "wasserstein",
+    "kendall_tau",
+    "mutual_information",
+    "bray_curtis",
+    "canberra_distance",
+    "mara_weighted_similarity",
+    "diagnostic_weighted_similarity",
+]
+
+
+def add_gaussian_noise(arr, mean=0, std_dev=0.01):
+    noise = np.random.normal(mean, std_dev, arr.shape)
+    return arr + noise
+
+
+def swap_two(arr):
+    idx1, idx2 = np.random.choice(len(arr), 2, replace=False)
+    arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+    return arr
+
+
+def metrics_comparison(
+    peptides_predictions,
+    peptides_switch_predictions,
+    num_randomization_rounds=1,
+    noise_mean=0,
+    noise_std_dev=0.001,
+    num_randomizations=1,
+    randomize_gaussian=False,
+    randomize_switched=False,
+):
+    peptide_dict = {}
+
+    # Iterate through unique peptide IDs
+    for i in set(peptides_predictions["ID"]):
+
+        selected_peptide = peptides_predictions[peptides_predictions["ID"] == i]
+        selected_peptide_switched = peptides_switch_predictions[
+            peptides_switch_predictions["ID"] == i
+        ]
+
+        common_annotations = set(selected_peptide["annotation"]).intersection(
+            selected_peptide_switched["annotation"]
+        )
+
+        selected_peptide = selected_peptide[
+            selected_peptide["annotation"].isin(common_annotations)
+        ].copy()
+
+        selected_peptide_switched = selected_peptide_switched[
+            selected_peptide_switched["annotation"].isin(common_annotations)
+        ].copy()
+
+        original_intensities = selected_peptide["intensities"].to_numpy()
+
+        for j in range(num_randomization_rounds):
+            score_dict = {}
+
+            # Maybe this should be renamed as these are not always noisy
+            noisy_intensities = selected_peptide["intensities"].to_numpy()
+            if randomize_gaussian:
+                # Add Gaussian noise instead of swapping
+                noisy_intensities = add_gaussian_noise(
+                    original_intensities, mean=noise_mean, std_dev=noise_std_dev
+                )
+            if randomize_switched:
+                noisy_intensities = selected_peptide["intensities"].to_numpy()
+                for _ in range(num_randomizations):
+                    noisy_intensities = swap_two(noisy_intensities)
+            noisy_intensities = np.clip(noisy_intensities, 0, None)
+
+            for key in metric_keys:
+                inp = {
+                    "intensity1": noisy_intensities,
+                    "intensity2": selected_peptide_switched["intensities"].to_numpy(),
+                    "mz1": selected_peptide["mz"].to_numpy(),
+                    "mz2": selected_peptide_switched["mz"].to_numpy(),
+                    "diagnostic_mz": np.array([]),
+                    "mz": selected_peptide["mz"].to_numpy(),
+                }
+
+                score = getattr(M, key)(**inp)
+
+                score_dict[key] = score
+
+            peptide_dict[
+                selected_peptide["peptide_sequences"].iloc[0]
+                + "|"
+                + selected_peptide_switched["peptide_sequences"].iloc[0]
+                + "|"
+                + str(j)
+            ] = score_dict
+
+    score_df = pd.DataFrame(peptide_dict).T
+    return score_df
