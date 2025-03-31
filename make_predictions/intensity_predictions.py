367d34702b75611396c0f80960f371ddd5a46e4b
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-03-31 11:24:57 +0200

    Full workflow

--- make_predictions/intensity_predictions.py
+++ make_predictions/intensity_predictions.py
@@ -4,16 +4,20 @@ from koinapy import Koina
 
 
 def obtain_predictions_pairs(
-    peptides, charges=[2], collision_energie=28, instrument_types="LUMOS"
+    peptides,
+    charges=[2],
+    collision_energie=28,
+    instrument_types="LUMOS",
+    switched=False,
 ):
     """
     Function to obtain intensity predictions for a set of peptides.
     """
-    num_peptides = peptides.shape[0] * 2
+    num_peptides = peptides.shape[0]
 
     inputs = pd.DataFrame()
-    inputs["peptide_sequences"] = np.array(peptides.flatten())
-    inputs["precursor_charges"] = np.array(num_peptides * [2])
+    inputs["peptide_sequences"] = np.array(peptides)
+    inputs["precursor_charges"] = np.array(num_peptides * charges)
     inputs["collision_energies"] = np.array(num_peptides * [collision_energie])
     inputs["instrument_types"] = np.array(num_peptides * [instrument_types])
 
@@ -23,4 +27,14 @@ def obtain_predictions_pairs(
     predictions["annotation"] = predictions["annotation"].map(
         lambda x: x.decode("utf-8")
     )
+
+    predictions["ID"] = (
+        predictions.groupby(
+            ["peptide_sequences", "precursor_charges"], sort=False
+        ).ngroup()
+        + 1
+    )
+
+    predictions["non_switched"] = switched
+
     return predictions
