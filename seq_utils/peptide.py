1474ebf264745b7b3317185fdd9d92b9382e250e
RobbinBouwmeester <robbin.bouwmeester@ugent.be>
2025-07-01 09:52:04 +0200

    Prep retention time eval

--- seq_utils/peptide.py
+++ seq_utils/peptide.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 
 def remove_non_il(peptides: list) -> list:
@@ -17,3 +18,47 @@ def remove_ux_containing(peptides: list) -> list:
 
 def switch_first_il(peptide):
     return re.sub(r"[IL]", lambda x: "L" if x.group() == "I" else "I", peptide, count=1)
+
+
+import re
+import random
+
+
+def switch_random_il(peptide):
+    """
+    Randomly swap an occurrence of I or L, ignoring any I or L inside square brackets.
+    If only a single occurrence is found outside brackets, it will be switched.
+    """
+    # Find all spans corresponding to brackets
+    bracket_spans = [m.span() for m in re.finditer(r"\[[^\]]*\]", peptide)]
+
+    def is_inside_brackets(position):
+        for start, end in bracket_spans:
+            if start <= position < end:
+                return True
+        return False
+
+    # Find positions of I and L that are NOT inside brackets
+    positions = [
+        m.start()
+        for m in re.finditer(r"[IL]", peptide)
+        if not is_inside_brackets(m.start())
+    ]
+
+    if not positions:
+        # No I or L found outside brackets
+        return peptide
+
+    if len(positions) == 1:
+        # Only one occurrence; switch it
+        pos = positions[0]
+    else:
+        # Randomly select one occurrence excluding the first
+        pos = random.choice(positions[1:])
+
+    # Perform the swap
+    swapped_char = "L" if peptide[pos] == "I" else "I"
+
+    # Reconstruct the peptide
+    peptide = peptide[:pos] + swapped_char + peptide[pos + 1 :]
+    return peptide
